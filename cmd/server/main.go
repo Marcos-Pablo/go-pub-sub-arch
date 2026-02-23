8e14288bc68c40ecf9efe06cb8436978587446a6
Marcos Pablo <marquin.pablo@gmail.com>
2026-02-23 00:03:20 -0300

    feat(pubsub): add JSON publisher and send pause message
    - Implemented a generic PublishJSON function in internal/pubsub/publish.go
      to publish JSON-encoded messages to RabbitMQ exchanges.
    - Updated cmd/server/main.go to use PublishJSON for sending a pause
      state message to the peril game server exchange.
    - Added error handling and confirmation log for publishing the pause
      message.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,11 +6,13 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	const rabbitConnString = ""
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
 
 	conn, err := amqp.Dial(rabbitConnString)
 	if err != nil {
@@ -18,6 +20,21 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		log.Fatalf("could not create channel: %v", err)
+	}
+
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+
+	if err != nil {
+		log.Printf("could not publish time: %s", err)
+	}
+	fmt.Println("Pause message sent!")
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
