482f7c3ba1fb8bb3f08379b7a93ea574e5bd6307
Marcos Pablo <marquin.pablo@gmail.com>
2026-02-23 01:11:39 -0300

    feat(pubsub): add DeclareAndBind for queue setup
    Add internal/pubsub/consume.go with DeclareAndBind function to simplify
    queue declaration and binding to exchanges. Integrate this function in
    client startup for pause queue setup and improve error handling and
    logging.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitConnString)
+
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Peril client connected to RabbitMQ!")
+
+	name, err := gamelogic.ClientWelcome()
+
+	if err != nil {
+		log.Fatalf("Error prompting username: %s", err)
+	}
+
+	queueName := fmt.Sprintf("pause.%s", name)
+
+	_, queue, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
+
+	if err != nil {
+		log.Fatalf("Error declaring and binding queue: %s", err)
+	}
+
+	fmt.Printf("Queue %s declared and bound!\n", queue.Name)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
